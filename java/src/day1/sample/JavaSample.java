38bac176d00b9e901dd222fe2fa90f09ce16fbd6
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-05-30 12:45:42 +0900

    자바 프로젝트 생성 및 자바 샘플 코드 추가

---
+++ java/src/day1/sample/JavaSample.java
@@ -0,0 +1,16 @@
+package day1.sample;
+
+public class JavaSample {
+
+	/**
+	 * 메소드 주석
+	 * @param args
+	 */
+	// main : 프로그램이 실행되기 위해 필요한 메소드
+	public static void main(String[] args) {
+		//콘솔창에 원하는 문구를 출력하는 기능을 가진 메소드
+		System.out.println("Hello Java!");
+
+	}
+
+}
