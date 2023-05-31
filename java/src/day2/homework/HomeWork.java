9b16deb89260e218265b527518ea3612adbdfbd1
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-05-31 15:20:22 +0900

    홈 예제

--- java/src/day2/homework/HomeWork.java
+++ java/src/day2/homework/HomeWork.java
@@ -3,7 +3,9 @@ package day2.homework;
 public class HomeWork {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num1 = 1, num2 = 2;
+		int sum = num1 + num2;
+		System.out.println("" + num1 + " + " + num2 + " = " + sum);
 
 	}
 
