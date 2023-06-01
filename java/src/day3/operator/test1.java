4f2ddbec41e06902fb0c833667ec35c2750766ac
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-06-01 13:37:46 +0900

    자바 : 3일차 연습 예제

---
+++ java/src/day3/operator/test1.java
@@ -0,0 +1,20 @@
+package day3.operator;
+
+import java.util.Scanner;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input score : ");
+		int score = sc.nextInt();
+		boolean result = score >= 60;
+		System.out.println("Do you pass " + score + " points?" + result);
+		
+		sc.close();
+
+		
+	}
+
+}
