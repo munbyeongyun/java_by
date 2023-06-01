dab184f714ce86061e1256e947baabdec4bec9a4
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-06-01 13:46:36 +0900

    3일차 예제

--- java/src/day3/operator/Test1.java
+++ java/src/day3/operator/Test1.java
@@ -2,17 +2,19 @@ package day3.operator;
 
 import java.util.Scanner;
 
-public class test2 {
+public class Test1 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("input score : ");
 		int score = sc.nextInt();
-		String result = score >= 60 ? "pass" : "false";
+		boolean result = score >= 60;
 		System.out.println("Do you pass " + score + " points?" + result);
 		
 		sc.close();
 
+		
 	}
 
 }
--- java/src/day3/operator/Test2.java
+++ java/src/day3/operator/Test2.java
@@ -2,19 +2,18 @@ package day3.operator;
 
 import java.util.Scanner;
 
-public class test1 {
+public class Test2 {
 
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("input score : ");
 		int score = sc.nextInt();
-		boolean result = score >= 60;
+		String result = score >= 60 ? "pass" : "false";
 		System.out.println("Do you pass " + score + " points?" + result);
 		
 		sc.close();
-
 		
+
 	}
 
 }
