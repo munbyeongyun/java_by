97d9e62ff44622a55472181240d3d594f535adc2
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-06-02 14:37:05 +0900

    if문 예제를 조건 연산자로 변환

---
+++ java/src/day4/ifex/EvenNumberEx2.java
@@ -0,0 +1,18 @@
+package day4.ifex;
+
+import java.util.Scanner;
+
+public class EvenNumberEx2 {
+
+	public static void main(String[] args) {
+		// 홀짝 판별 예제를 조건 연산자를 이용하여 작성
+		int num = 4;
+		String result = num % 2 == 0 ? "even" : "odd";
+		
+		System.out.println(num + " is a" + result + " number.");
+
+
+
+	}
+
+}
