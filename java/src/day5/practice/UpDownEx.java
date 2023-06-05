09ecebca4a1e7e74e6736e050a35d1d2a937ca0d
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-06-05 14:56:33 +0900

    day5 homework

---
+++ java/src/day5/practice/UpDownEx.java
@@ -0,0 +1,17 @@
+package day5.practice;
+
+public class UpDownEx {
+
+	public static void main(String[] args) {
+		// 1~100사이의 랜덤한 정수를 생성하고, 이 정수를 맞추는 게임을 구현하세요.
+		// 랜덤한 수 : 45
+		// input : 50
+		// Down!
+		// input : 40
+		// Up!
+		// input : 45
+		// Good!
+
+	}
+
+}
