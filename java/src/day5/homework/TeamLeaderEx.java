4174be759cbab6ee9c55b861d01a9bffdb3e130f
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-06-05 15:18:57 +0900

    day5 예제 1

--- java/src/day5/homework/TeamLeaderEx.java
+++ java/src/day5/homework/TeamLeaderEx.java
@@ -1,5 +1,6 @@
 package day5.homework;
 
+
 public class TeamLeaderEx {
 
 	public static void main(String[] args) {
@@ -8,7 +9,18 @@ public class TeamLeaderEx {
 		// 학생 번호가 주어졌을 때 조장인지 조원인지 판별하는 코드를 작성하세요.
 		// switch문으로
 		
+		int num = 1;
+		int min =1, max = 30;
+		int random =(int)(Math.random() * (max - min + 1) + min);
 		
+		switch(random % 3) {
+		case 0:
+			System.out.println(random +" 팀리더");
+			break;
+		default:
+			
+			System.out.println(random + " 조원");
+		}		
 
 	}
 
