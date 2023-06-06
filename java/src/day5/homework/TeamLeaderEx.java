3865e35b36fb53bb50c9a103d5f6c8a80fbf9754
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-06-07 08:47:48 +0900

    Update TeamLeaderEx.java

--- java/src/day5/homework/TeamLeaderEx.java
+++ java/src/day5/homework/TeamLeaderEx.java
@@ -9,7 +9,6 @@ public class TeamLeaderEx {
 		// 학생 번호가 주어졌을 때 조장인지 조원인지 판별하는 코드를 작성하세요.
 		// switch문으로
 		
-		int num = 1;
 		int min =1, max = 30;
 		int random =(int)(Math.random() * (max - min + 1) + min);
 		
