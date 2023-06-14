2c0a01d6ec09e1fb1505b73339a7e65ccfe67a52
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-06-14 14:55:51 +0900

    자바 : 배열 랜덤 저장 예제

---
+++ java/src/day11/practice/ArrayRandomEx.java
@@ -0,0 +1,20 @@
+package day11.practice;
+
+public class ArrayRandomEx {
+
+	public static void main(String[] args) {
+		// 1~9사이의 랜덤한 수를 3개 만들어서 배열에 저장한 후 출력하는 코드를 작성하세요.
+		
+		int min =1, max = 9;
+		
+		
+		int [] randomArray = new int[3];
+		
+		
+		for (int i = 0; i < 3; i++) {
+			randomArray [i] = (int)(Math.random() * (max - min + 1) + min);
+			System.out.println(randomArray [i]);
+		}
+	}
+
+}
