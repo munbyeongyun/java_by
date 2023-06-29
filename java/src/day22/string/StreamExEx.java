84c86b82bb03625fc838b724430386af47fcd470
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-06-29 13:59:55 +0900

    자바 : 람다식 예제

---
+++ java/src/day22/string/StreamExEx.java
@@ -0,0 +1,37 @@
+package day22.string;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamExEx {
+
+	public static void main(String[] args) {
+		Integer arr[] = {1,2,3,4,5};
+		List<Integer> list = Arrays.asList(1,2,3,4,5);
+		
+		Stream<Integer> arrStream = Arrays.stream(arr);
+		Stream<Integer> listStream = list.stream();
+		
+		
+		//전체 출력
+		arrStream.forEach(num->System.out.println(num));
+		System.out.println("=============");
+		listStream.forEach(num->System.out.println(num));
+		System.out.println("=============");
+		
+		//3보다 큰 수
+		arrStream = Arrays.stream(arr);
+		arrStream
+		.filter(num -> num>3)
+		.forEach(num -> System.out.println(num));
+		
+		System.out.println("=============");
+		
+		listStream = list.stream();
+		listStream
+		.filter(num -> num>3)
+		.forEach(num -> System.out.println(num));
+	}
+
+}
