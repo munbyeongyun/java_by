750f043322193876a8fb6482eb3142f07acb59e8
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-06-29 14:02:41 +0900

    자바 : Stream 예제

--- java/src/day22/stream/StreamEx.java
+++ java/src/day22/stream/StreamEx.java
@@ -1,10 +1,10 @@
-package day22.string;
+package day22.stream;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class StreamExEx {
+public class StreamEx {
 
 	public static void main(String[] args) {
 		Integer arr[] = {1,2,3,4,5};
