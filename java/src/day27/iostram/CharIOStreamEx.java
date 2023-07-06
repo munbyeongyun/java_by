e64b6ba58bef7545951c6c65679c9fa093d1b5cb
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-07-06 14:50:43 +0900

    day 27 FileWriter 예제

--- java/src/day27/iostram/CharIOStreamEx.java
+++ java/src/day27/iostram/CharIOStreamEx.java
@@ -21,6 +21,8 @@ public class CharIOStreamEx {
 			System.out.println("Exception!");
 		}
 		//문자 단위로 쓰는 예제
+		//FileWriter생성자(파일명, 이어쓰기여부) : 이어쓰기 여부가 true이면 이어서 작성
+		//FileWriter생성자(파일명) : 이어쓰기 여부가 false로 설정
 		try(FileWriter fw = new FileWriter("file.txt", true)) {
 			String str = "Hello World!\n";
 			fw.write(str);
