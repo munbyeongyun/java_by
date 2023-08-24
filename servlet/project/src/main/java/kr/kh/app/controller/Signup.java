d2e1d2f6f5dc70c3c37d31913025410ef1341883
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-08-24 17:21:15 +0900

    Servlet : 회원가입 및 회원 탈퇴 기능 구현 완료

--- servlet/project/src/main/java/kr/kh/app/controller/Signup.java
+++ servlet/project/src/main/java/kr/kh/app/controller/Signup.java
@@ -10,14 +10,14 @@ import kr.kh.app.service.MemberService;
 import kr.kh.app.service.MemberServiceImp;
 import kr.kh.app.vo.MemberVO;
 
-/**
- * Servlet implementation class Singup
- */
 public class Signup extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-    private MemberService memberService = new MemberServiceImp();
-    public Signup() {
-        super();
+	private MemberService memberService = new MemberServiceImp();
+
+	public Signup() {
+        super(); 
+    
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -31,7 +31,7 @@ public class Signup extends HttpServlet {
 		boolean ok = false;
 		if(memberService.signup(member)) {
 			ok = true;
-		}
+		} 
 		request.setAttribute("signupOk", ok);
 		doGet(request, response);
 	}
