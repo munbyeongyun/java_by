242afd46da31b00888d879a5475406d018816182
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-09-06 17:45:36 +0900

    SPRING : 검색창, 게시글, 댓글 등록 후 알람 및 기타 처리

--- spring projects/study/src/main/java/kr/kh/study/controller/BoardController.java
+++ spring projects/study/src/main/java/kr/kh/study/controller/BoardController.java
@@ -9,9 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import kr.kh.study.pagination.Criteria;
+import kr.kh.study.pagination.PageMaker;
 import kr.kh.study.service.BoardService;
 import kr.kh.study.vo.BoardVO;
+import kr.kh.study.vo.FileVO;
 import kr.kh.study.vo.MemberVO;
 
 @Controller
@@ -21,11 +25,16 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@GetMapping("/board/list")
-	public String boardList(Model model) {
+	public String boardList(Model model, Criteria cri) {
+		cri.setPerPageNum(2);
 		//서비스에게 게시글 리스트를 가져오라고 시킴 
-		List<BoardVO> list = boardService.getBoardList();
+		List<BoardVO> list = boardService.getBoardList(cri);
+		int totalCount = boardService.getBoardTotalCount();
+		
+		PageMaker pm = new PageMaker(3, cri, totalCount);
 		//가져온 리스트를 화면에 전송 
 		model.addAttribute("list", list);
+		model.addAttribute("pm", pm);
 		return "/board/list";
 	}
 	@GetMapping("/board/detail")
@@ -34,8 +43,10 @@ public class BoardController {
 		boardService.updateViews(bo_num);
 		//서비스에게 게시글 번호를 주면서 게시글을 가져오라고 요청
 		BoardVO board = boardService.getBoard(bo_num);
+		List<FileVO> fileList = boardService.getFileList(bo_num);
 		//가져온 게시글을 화면에 전송
 		model.addAttribute("board", board);
+		model.addAttribute("fileList", fileList);
 		return "/board/detail";
 	}
 	@GetMapping("/board/insert")
@@ -43,10 +54,10 @@ public class BoardController {
 		return "/board/insert";
 	}
 	@PostMapping("/board/insert")
-	public String boardInserPost(Model model, BoardVO board, HttpSession session) {
+	public String boardInserPost(Model model, BoardVO board, HttpSession session, MultipartFile[] files) {
 		MemberVO user = (MemberVO) session.getAttribute("user");
 		
-		boolean res = boardService.insertBoard(board, user);
+		boolean res = boardService.insertBoard(board, user, files);
 		if(res) {
 			model.addAttribute("msg", "게시글을 등록했습니다.");
 			model.addAttribute("url", "/board/list");
@@ -60,13 +71,17 @@ public class BoardController {
 	@GetMapping("/board/update")
 	public String boardUpdate(Model model,Integer bo_num) {
 		BoardVO board = boardService.getBoard(bo_num);
+		List<FileVO> fileList = boardService.getFileList(bo_num);
+		//가져온 게시물을 화면에 전송
 		model.addAttribute("board", board);
+		model.addAttribute("fileList", fileList);
 		return "/board/update";
 	}
 	@PostMapping("/board/update")
-	public String boardUpdatePost(Model model, BoardVO board, HttpSession session) {
+	public String boardUpdatePost(Model model, BoardVO board, HttpSession session,
+			MultipartFile[] files, int[] delNums) {
 		MemberVO user = (MemberVO)session.getAttribute("user");
-		boolean res = boardService.update(board,user);
+		boolean res = boardService.update(board, user, files, delNums);
 		if(res) {
 			model.addAttribute("msg", "게시글을 수정했습니다.");
 		}else {
--- spring projects/study/src/main/java/kr/kh/study/service/BoardServiceImp.java
+++ spring projects/study/src/main/java/kr/kh/study/service/BoardServiceImp.java
@@ -1,12 +1,18 @@
 package kr.kh.study.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.kh.study.dao.BoardDAO;
+import kr.kh.study.pagination.Criteria;
+import kr.kh.study.util.UploadFileUtils;
 import kr.kh.study.vo.BoardVO;
+import kr.kh.study.vo.FileVO;
 import kr.kh.study.vo.MemberVO;
 
 @Service
@@ -14,12 +20,17 @@ public class BoardServiceImp implements BoardService{
 
 	@Autowired
 	BoardDAO boardDao;
+	
+	String uploadPath = "D:\\uploadfiles";
 
 	@Override
-	public List<BoardVO> getBoardList() {
-		//매개변수체크(생략)
+	public List<BoardVO> getBoardList(Criteria cri) {
+		//매개변수체크(생략)cri 넣으면서 체크
+		if(cri == null) {
+			cri = new Criteria();
+		}
 		//다오에게 게시글 리스트를 가져오라고 시키고
-		List<BoardVO> list = boardDao.selectBoardList();
+		List<BoardVO> list = boardDao.selectBoardList(cri);
 		//가져오면 반환
 		return list;
 	}
@@ -47,7 +58,7 @@ public class BoardServiceImp implements BoardService{
 	}
 
 	@Override
-	public boolean insertBoard(BoardVO board, MemberVO user) {
+	public boolean insertBoard(BoardVO board, MemberVO user, MultipartFile[] files) {
 		if(user == null || user.getMe_id() == null) {
 			return false;
 		}
@@ -56,11 +67,38 @@ public class BoardServiceImp implements BoardService{
 		}
 		board.setBo_me_id(user.getMe_id());
 		boolean res = boardDao.insertBoard(board);
-		return res;
+		if(!res) {
+			return false;
+		}
+		//첨부파일 추가
+		uploadFiles(files, board.getBo_num());
+		return true;
+	}
+
+	private void uploadFiles(MultipartFile[] files, int bo_num) {
+		if(files == null || files.length == 0) {
+			return;
+		}
+		if(bo_num <= 0) {
+			return;
+		}
+		for(MultipartFile file : files) {
+			if(file == null || file.getOriginalFilename().length() == 0) {
+				continue;
+			}
+			try {
+				String fi_ori_name = file.getOriginalFilename();
+				String fi_name = UploadFileUtils.uploadFile(uploadPath, fi_ori_name, file.getBytes());
+				FileVO fileVo = new FileVO(fi_name, fi_ori_name, bo_num);
+				boardDao.insertFile(fileVo);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
-	public boolean update(BoardVO board, MemberVO user) {
+	public boolean update(BoardVO board, MemberVO user, MultipartFile[] files, int[] delNums) {
 		if(user == null || user.getMe_id() == null) {
 			return false;
 		}
@@ -71,8 +109,24 @@ public class BoardServiceImp implements BoardService{
 		if(dbBoard == null || !dbBoard.getBo_me_id().equals(user.getMe_id())) {
 			return false;
 		}
+		//추가된 첨부파일 업로드 및 DB추가
+		uploadFiles(files, board.getBo_num());
+		//삭제된 첨부파일 삭제 및 DB제거
+		deleteFiles(delNums);
 		boolean res = boardDao.updateBoard(board);
 		return res;
+	}
+
+	private void deleteFiles(int[] delNums) {
+		if(delNums == null || delNums.length == 0) {
+			return;
+		}
+		List<FileVO> fileList = new ArrayList<FileVO>();
+		for(int fi_num : delNums) {
+			FileVO filVo = boardDao.selectFile(fi_num);
+			fileList.add(filVo);
+		}
+		deleteFiles(fileList);
 	}
 
 	@Override
@@ -87,7 +141,38 @@ public class BoardServiceImp implements BoardService{
 		if(board == null || !board.getBo_me_id().equals(user.getMe_id())) {
 			return false;
 		}
+		//첨부파일을 삭제
+		//게시글의 모든 첨부파일들을 가져옴
+		List<FileVO> fileList = boardDao.selectFileList(bo_num);
+		//첨부파일 정보를 주면서 삭제하라고 요청
+		deleteFiles(fileList);
 		return boardDao.deleteBoard(bo_num);
+	}
+
+	private void deleteFiles(List<FileVO> fileList) {
+		if(fileList == null || fileList.size() == 0) {
+			return;
+		}
+		for(FileVO file : fileList) {
+			if(file == null) {
+				continue;
+			}
+			UploadFileUtils.deleteFile(uploadPath, file.getFi_name());
+			boardDao.deleteFile(file.getFi_num());
+		}
+	}
+
+	@Override
+	public List<FileVO> getFileList(Integer bo_num) {
+		if(bo_num == null) {
+			return null;
+		}
+		return boardDao.selectFileList(bo_num);
+	}
+
+	@Override
+	public int getBoardTotalCount() {
+		return boardDao.selectBoardTotalCount();
 	}
 	
 	
