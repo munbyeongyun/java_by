d630837324b1d3672a181b890755b7d556670c6d
munbyeongyun <83917908+munbyeongyun@users.noreply.github.com>
2023-09-07 12:40:55 +0900

    SPRING : 댓글 등록/삭제 시 게시글에 댓글 수 변경 적용

--- spring projects/spring/src/main/java/kr/kh/spring/service/CommentService.java
+++ spring projects/spring/src/main/java/kr/kh/spring/service/CommentService.java
@@ -1,9 +1,18 @@
 package kr.kh.spring.service;
 
+import java.util.List;
+
+import kr.kh.spring.pagination.Criteria;
 import kr.kh.spring.vo.CommentVO;
 
 public interface CommentService {
 
 	boolean insertComment(CommentVO comment);
+
+	List<CommentVO> getCommentList(Criteria cri, int bo_num);
+
+	int getTotalCount(int bo_num);
+
+	boolean deleteComment(CommentVO comment);
 
 }
